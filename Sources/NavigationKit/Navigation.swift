1db882147b373ebbb86023cb050620f1ce2d7a51
Alex Nagy <therebeloper@gmail.com>
2020-12-15 00:21:21 +0200

    Navigation

--- Sources/NavigationKit/Navigation.swift
+++ Sources/NavigationKit/Navigation.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-public class Navigation: ObservableObject {
+class Navigation: ObservableObject {
     @Published var isPushRootActive: Bool = false
-    public init() { }
 }
