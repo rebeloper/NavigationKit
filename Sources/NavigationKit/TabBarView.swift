f2652517b051203b5ff1341b377a8ea0cf89a824
Alex Nagy <therebeloper@gmail.com>
2020-12-15 09:31:30 +0200

    init

--- Sources/NavigationKit/TabBarView.swift
+++ Sources/NavigationKit/TabBarView.swift
@@ -15,6 +15,15 @@ public struct TabBarView<RootView: View, TabItemView: View>: View {
     public var navigation: Navigation = Navigation()
     public var tabTag: Int
     
+    public init(rootView: @escaping () -> RootView, tabItemView: @escaping () -> TabItemView, navigation: Navigation? = nil, tabTag: Int) {
+        self.rootView = rootView
+        self.tabItemView = tabItemView
+        if let navigation = navigation {
+            self.navigation = navigation
+        }
+        self.tabTag = tabTag
+    }
+    
     public var body: some View {
         NavigationView {
             rootView()
