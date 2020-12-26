ed030dd16706911c5d839c1c8a889a8ce1aee681
Alex Nagy <therebeloper@gmail.com>
2020-12-26 11:42:40 +0200

    tweak

--- Sources/NavigationKit/ButtonAsRootNavigationLink.swift
+++ Sources/NavigationKit/ButtonAsRootNavigationLink.swift
@@ -22,6 +22,7 @@ public struct ButtonAsRootNavigationLink<Destination: View, Content: View>: View
 
     public var body: some View {
         Button(action: {
+            isActive = true
             pushingInRoot?.wrappedValue = true
         }, label: { content })
             .background(
