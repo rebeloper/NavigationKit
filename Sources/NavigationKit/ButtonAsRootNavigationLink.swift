6b7daa6c781ce290bbd3c630b3f6ba05d4ea8c7c
Alex Nagy <therebeloper@gmail.com>
2020-12-26 12:12:18 +0200

    tweak

--- Sources/NavigationKit/ButtonAsRootNavigationLink.swift
+++ Sources/NavigationKit/ButtonAsRootNavigationLink.swift
@@ -12,25 +12,25 @@ public struct ButtonAsRootNavigationLink<Destination: View, Content: View>: View
     public let destination: Destination
     public let content: Content
 
-    @Binding public var isActive: Bool
+    public var isActive: Binding<Bool>
     @Environment(\.pushingInRoot) var pushingInRoot
 
     public init(destination: Destination, @ViewBuilder content: () -> Content, isActive: Binding<Bool>) {
         self.destination = destination
         self.content = content()
-        self._isActive = isActive
+        self.isActive = isActive
     }
 
     public var body: some View {
         Button(action: {
-            isActive = true
+            isActive.wrappedValue = true
         }, label: { content })
             .background(
                 NavigationLink(
                     destination: destination,
-                    isActive: $isActive,
+                    isActive: isActive,
                     label: { EmptyView() }
                 ).isDetailLink(false).hidden()
-            ).environment(\.pushingInRoot, $isActive)
+            ).environment(\.pushingInRoot, isActive)
     }
 }
