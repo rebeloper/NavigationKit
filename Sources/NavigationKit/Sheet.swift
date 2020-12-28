88dc848fbbd9ed735f1dded6483a67a1b409dc87
Alex Nagy <therebeloper@gmail.com>
2020-12-28 17:49:58 +0200

    onDismiss

--- Sources/NavigationKit/Sheet.swift
+++ Sources/NavigationKit/Sheet.swift
@@ -10,15 +10,17 @@ import SwiftUI
 public struct Sheet<Content>: View where Content: View {
     
     @Binding public var isPresented: Bool
+    public let onDismiss: (() -> Void)?
     public let content: () -> Content
     
-    public init(isPresented: Binding<Bool>, content: @escaping () -> Content) {
+    public init(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, content: @escaping () -> Content) {
         self._isPresented = isPresented
+        self.onDismiss = onDismiss
         self.content = content
     }
 
     public var body: some View {
-        EmptyView().sheet(isPresented: $isPresented, content: {
+        EmptyView().sheet(isPresented: $isPresented, onDismiss: onDismiss, content: {
             content()
         })
     }
