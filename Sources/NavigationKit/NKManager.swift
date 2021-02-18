f02b7b9f519b003fd4b545045944e3a9a7793c90
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-18 10:39:58 +0200

    dismiss

--- Sources/NavigationKit/NKManager.swift
+++ Sources/NavigationKit/NKManager.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 public struct NKManager {
+    public static func dismiss(with presentationMode: Binding<Bool>) {
+        presentationMode.wrappedValue.dismiss()
+    }
+    
     public static func dismissTwo(with presentationsMode: [Binding<Bool>]) {
         presentationsMode.forEach {
             $0.wrappedValue.dismiss()
